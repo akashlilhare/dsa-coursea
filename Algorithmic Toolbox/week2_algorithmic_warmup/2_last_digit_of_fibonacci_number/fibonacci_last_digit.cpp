972a9767c0dce2c7a2b13de46e2c549f06a18aba
akashlilhare <akashlilhare14@gmail.com>
2021-02-18 23:05:56 +0530

    sub-2

--- Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -16,9 +16,23 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
+int get_fibonacci_number(int n){
+    int arr[n] ={0};
+    arr[0] = 0;
+    arr[1] = 1;
+    for(int i=2; i<=n; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n];
+}
+
+int get_fibonacci_last_digit_fast(int n){
+    return get_fibonacci_number(n) %10;
+}
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
-    std::cout << c << '\n';
-    }
+   // int c = get_fibonacci_last_digit_naive(n);
+   // std::cout << c << '\n';
+    std::cout<<get_fibonacci_last_digit_fast(n);
+ }
