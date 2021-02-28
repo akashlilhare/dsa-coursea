2f020235701d68f8062cbbb9856558c0aff08b46
akashlilhare <akashlilhare14@gmail.com>
2021-03-01 00:43:26 +0530

    week_2_solution_8

--- Algorithmic Toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ Algorithmic Toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -1,25 +1,44 @@
 #include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+typedef   long long int               ll;
 
-int fibonacci_sum_squares_naive(long long n) {
-    if (n <= 1)
-        return n;
+//freopen("input.txt","r",stdin);
+//freopen("output.txt","w",stdout);
 
-    long long previous = 0;
-    long long current  = 1;
-    long long sum      = 1;
+using namespace std;
 
-    for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
-        sum += current * current;
-    }
+ll Periodic(ll m) {
+	ll a = 0, b = 1, c = a + b;
+	for (int i = 0; i < m*m; i++) {
+		c = (a + b) % m;
+		a = b;
+		b = c;
+		if (a == 0 && b == 1)
+			return i + 1;
+	}
+}
+int Solve(ll n, ll m) {
+	long long remainder = n % Periodic(m);
 
-    return sum % 10;
+	long long first = 0;
+	long long second = 1;
+
+	long long res = remainder;
+
+	for (int i = 1; i < remainder; i++) {
+		res = (first + second) % m;
+		first = second;
+		second = res;
+	}
+
+	return res % m;
 }
 
+// Sum Of (Fib[0]^2+Fib[1]^2+.....+Fib[n]^2) = Fib[n]*Fib[n+1]
 int main() {
-    long long n = 0;
-    std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+	ll n; 
+	cin >> n;
+	cout << (Solve(n + 1,10)*Solve(n,10)) % 10 << endl;
 }
