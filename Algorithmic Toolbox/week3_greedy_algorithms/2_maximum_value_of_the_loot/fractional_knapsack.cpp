f967fd938c070962082d4833b9a21a017f148a50
akashlilhare <akashlilhare14@gmail.com>
2021-03-02 23:42:25 +0530

    fractional knapsack

--- Algorithmic Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ Algorithmic Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 using std::vector;
 
+bool cmp(pair<int,int> a, pair<int,int> b){
+  double r1 = a.second / a.first;
+  double r2 = b.second / b.first;
+
+  return r1 > r2;
+}
+
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
-  double value = 0.0;
 
-  // write your code here
+  int n = weights.size();
+  vector<pair<int,int>> itemPair;
 
-  return value;
+  for(int i=0; i< n; i++)
+    itemPair.push_back(make_pair(weights[i], values[i]));
+
+  sort(itemPair.begin(), itemPair.end(), cmp);
+
+  //for(int i=0; i<n; i++)
+    //cout<<itemPair[i].second/itemPair[i].first<<"\t"<<"("<<itemPair[i].first<<","<<itemPair[i].second<<")"<<endl;
+  
+  double weight =0.0;
+  double price = 0.0;
+
+  for(int i=0; i<n; i++){
+    if(itemPair[i].first+weight <= capacity){
+      price += itemPair[i].second;
+      weight += (double)itemPair[i].first;
+    }else{
+      int remain = capacity - weight;
+      price  = price + (double)itemPair[i].second * remain;
+      break;
+    }
+  }
+
+  return price;
 }
 
 int main() {
