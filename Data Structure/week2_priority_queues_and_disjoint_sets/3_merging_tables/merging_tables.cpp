63e39bc7245cab2e9d7f193e32d83c5c4f6c01b3
akashlilhare <akashlilhare14@gmail.com>
2021-03-22 04:04:16 +0530

    merging tables

--- Data Structure/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.cpp
+++ Data Structure/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.cpp
@@ -1,20 +1,17 @@
-#include <cstdio>
-#include <cstdlib>
-#include <vector>
-#include <algorithm>
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::max;
-using std::vector;
 
 struct DisjointSetsElement {
 	int size, parent, rank;
-	
-	DisjointSetsElement(int size = 0, int parent = -1, int rank = 0):
-	    size(size), parent(parent), rank(rank) {}
+
+	DisjointSetsElement(int sz=0, int pt = -1, int rk = 0)
+	{
+	    size=sz;
+	    parent=pt;
+	    rank=rk;
+	}
 };
 
 struct DisjointSets {
@@ -22,45 +19,86 @@ struct DisjointSets {
 	int max_table_size;
 	vector <DisjointSetsElement> sets;
 
-	DisjointSets(int size): size(size), max_table_size(0), sets(size) {
+	DisjointSets(int sz)
+	{	size=sz;
+		max_table_size=0;
+		sets.resize(size);
 		for (int i = 0; i < size; i++)
 			sets[i].parent = i;
 	}
 
+
 	int getParent(int table) {
 		// find parent and compress path
+		int i=table;
+		if(i!=sets[i].parent)
+		sets[i].parent=getParent(sets[i].parent);
+
+		return sets[i].parent;
+
 	}
 
 	void merge(int destination, int source) {
+
 		int realDestination = getParent(destination);
 		int realSource = getParent(source);
 		if (realDestination != realSource) {
 			// merge two components
 			// use union by rank heuristic
                         // update max_table_size
-		}		
+			if(sets[realSource].rank>sets[realDestination].rank)
+			{
+				sets[realDestination].parent=realSource;
+				sets[realSource].size+=sets[realDestination].size;
+				sets[realDestination].size=0;
+				max_table_size=max(max_table_size,sets[realSource].size);
+			}
+			else
+			{
+				sets[realSource].parent=realDestination;
+				sets[realDestination].size+=sets[realSource].size;
+				sets[realSource].size=0;
+				max_table_size=max(max_table_size,sets[realDestination].size);
+
+				if(sets[realSource].rank==sets[realDestination].rank)
+					sets[realDestination].rank+=1;
+			}
+		}
+	}
+	void input()
+	{
+		for (int i=0;i<size;++i) {
+		cin >> sets[i].size;
+		max_table_size = max(max_table_size, sets[i].size);
+	}
 	}
 };
 
 int main() {
+	//ios_base::sync_with_stdio(false);
+	//cin.tie(0);
+	//cout.tie(0);
+	vector<int> ans;
 	int n, m;
 	cin >> n >> m;
 
 	DisjointSets tables(n);
-	for (auto &table : tables.sets) {
-		cin >> table.size;
-		tables.max_table_size = max(tables.max_table_size, table.size);
-	}
 
+	tables.input();
+
+	int destination, source;
 	for (int i = 0; i < m; i++) {
-		int destination, source;
+
 		cin >> destination >> source;
                 --destination;
                 --source;
-		
+
 		tables.merge(destination, source);
-	        cout << tables.max_table_size << endl;
+	       ans.push_back(tables.max_table_size)  ;
 	}
 
+	for(auto i : ans){
+		cout<<i<<endl;
+	}
 	return 0;
 }
