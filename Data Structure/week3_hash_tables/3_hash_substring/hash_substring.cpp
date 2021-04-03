fedc891ea6c5a03572570e7fe26b37c4b1845687
Akash Lilhare <akashlilhare14@gmail.com>
2021-04-04 04:03:56 +0530

    hash substring

--- Data Structure/week3_hash_tables/3_hash_substring/hash_substring.cpp
+++ Data Structure/week3_hash_tables/3_hash_substring/hash_substring.cpp
@@ -1,118 +1,145 @@
-  
-#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
 
-/*********************************
-******                      ******
-***        Ayran Olckers       ***
-**                              **
-**            07/2019           **
-******                      ******
-**********************************/
+using std::string;
+using namespace std::chrono;
+using namespace std;
+
+typedef unsigned long long ull;
 
 
-long long hash(const std::string &s, const long long &p, const long long &x) {
-    long long hash{0};
-    // hash = sum(s[i] * x^i mod p)
-    for (int i = s.size() - 1; i >= 0; --i) {
-        hash = (hash * x + s[i]) % p;
-    }
-    return hash;
+struct Data {
+    string pattern, text;
+};
+
+Data read_input() {
+    Data data;
+    std::cin >> data.pattern >> data.text;
+    return data;
 }
 
-std::vector<long long> compute_hashes(const std::string &text, const size_t psize,
-                                      const long long &p, const long long &x) {
-    // Dynamically compute hashes to save time.
-    size_t size = text.size() - psize + 1;
-    std::vector<long long> hashes(size);
-    // Initialize last hash
-    auto begin = text.begin() + size - 1;
-    auto end = text.end();
-    std::string last(begin, end);
-    hashes[size - 1] = hash(last, p, x);
-    // Dynamically compute hashes: h_{k-1} = x*h_k + text_{k-1} - text_{k+p-1}*x^p mod p
-    // Precoumpute x^psize
-    long long x_to_psize = 1;
-    for (int i = 1; i <= psize; ++i) {
-        x_to_psize = (x_to_psize * x) % p;
-    }
-    for (int k = (int)size - 1; k > 0; --k) {
-        hashes[k - 1] = (x * hashes[k] + text[k - 1] - text[k + psize - 1] * x_to_psize) % p;
-        // Normalize mod p value to positive representative.
-        hashes[k - 1] = (hashes[k - 1] + p) % p;
-    }
-    return hashes;
+void print_occurrences(const std::vector<int>& output) {
+    for (int i = output.size()-1; i >= 0; --i)
+        std::cout << output[i] << " ";
+    std::cout << "\n";
 }
 
-// Rabin-Karp's algorithm for searching pattern in a text
-std::vector<size_t> rabin_karp(const std::string &pattern, const std::string &text) {
-    // Choose a prime and number x in 1, prime - 1 for hashing function
-    long long p{1299709};
-    long long x{23};
-    // Precompute hashes of substrings
-    size_t psize = pattern.size();
-    long long phash = hash(pattern, p, x);
-    auto hashes = compute_hashes(text, psize, p, x);
-    // Compare substrings via their hashes
-    std::vector<size_t> indexes;
-    for (size_t idx = 0; idx < hashes.size(); ++idx) {
-        if (hashes[idx] == phash) {
-            // Get substring in text to compare
-            auto first = text.begin() + idx;
-            auto last = text.begin() + idx + psize;
-            std::string compare(first, last);
-            if (compare == pattern) {
-                // Found a match!
-                indexes.push_back(idx);
-            }
-        } else {
-            // If hashes are different, substrings can't match. Do nothing.
-        }
-    }
-    return indexes;
-}
-
-std::vector<size_t> naive_matching(const std::string &pattern, const std::string &text) {
-    std::vector<size_t> ans;
-    for (size_t i = 0; i + pattern.size() <= text.size(); ++i)
-        if (text.substr(i, pattern.size()) == pattern)
+std::vector<int> get_occurrences(const Data& input) {
+    const string& s = input.pattern, t = input.text;
+    std::vector<int> ans;
+    for (size_t i = 0; i + s.size() <= t.size(); ++i)
+        if (t.substr(i, s.size()) == s)
             ans.push_back(i);
     return ans;
 }
 
-// Helper function to output a vector in the console.
-template <class T> void display(const std::vector<T> &vector) {
-    for (auto &v : vector) {
-        std::cout << v << " ";
-    }
-    std::cout << std::endl;
+ull hash_func(const string& s,const ull &prime, const ull &x)
+{
+	unsigned long long hash = 0;
+	for (int i = static_cast<int> (s.size()) - 1; i >= 0; --i)
+		//hash = (((hash * x + s[i]) % prime) + prime) % prime;
+		hash = (hash * x + s[i]) % prime;
+	return hash; // % bucket_count;
 }
 
-bool test_solution(const std::string &pattern, const std::string &text) {
-    auto positions = rabin_karp(pattern, text);
-    auto positions_naive = naive_matching(pattern, text);
-    std::cout << "Rabin-Karp: ";
-    display(positions);
-    std::cout << "Naive: ";
-    display(positions_naive);
-    bool correct = (positions == positions_naive);
-    if (correct) {
-        std::cout << "OK!" << std::endl;
-    } else {
-        std::cout << "FAIL!" << std::endl;
-        std::cout << pattern << std::endl << text << std::endl;
-    }
-    return correct;
+std::vector<ull> PrecomputeHash(const string& T, const string& t_substr, const size_t &pattern_size, const ull &prime, const ull &x)
+{
+	size_t text_size = T.size();
+	vector<ull> H(text_size - pattern_size + 1);
+
+	H[text_size - pattern_size] = hash_func(t_substr, prime, x);
+	
+	
+	ull y = 1;
+	for (size_t i = 1; i <= pattern_size; ++i)
+	{
+		y = (y * x) % prime;
+	}
+	
+
+	for (int i = text_size - pattern_size - 1; i >= 0; --i)
+	{
+		H[i] = (ull)(((H[i + 1] * x) + T[i] - (y * T[i + pattern_size])) % prime);
+		//H[i] = (ull)((H[i] + prime) % prime);
+		//H[i] = (((H[i + 1] * x + T[i]) % prime - (y * T[i + pattern_size]) % prime) + prime) % prime;
+	}
+	return H;
 }
 
-int main() {
-    std::string pattern, text;
-    std::cin >> pattern;
-    std::cin >> text;
-    // test_solution(pattern, text);
-    auto positions = rabin_karp(pattern, text);
-    display(positions);
+void RibinKarp(const Data& input) {
+	// parse the input;
+	const string& p = input.pattern, t = input.text;
+
+	/*The hash functions should be selected at random, but once and for all keys. 
+	If we select a random hash function again for each key x we want to hash, 
+	this will be a non-deterministic hash function, which is not useful.
+	*/
+	ull prime = 100000007; // We can try bigger prime numbers to minimize the probability of missing the substring and false alarms/false positives.
+	ull x = 1;
+	// We could just select an X from 1 to prime-1, but we can also Randomize it
+	//std::srand(std::time(0)); //use current time as seed for random generator
+	
+	/*
+	while (x > (prime-1))
+		//x = ((double)rand() / (RAND_MAX)) * prime
+		x = 1 + std::rand() / ((RAND_MAX + 1u) / (prime-1));  // Note: 1+rand()%(prime-1) is biased
+	*/
+    //x = ull(((double)rand() / (RAND_MAX)) * (prime-1));
+
+	// initializing the answer list
+	//std::vector<int> ans;
+
+
+	// LET'S START THE FIND THE PATTERNS
+	size_t pattern_size = p.size();
+	size_t text_size = t.size();
+
+	ull pattern_hash = hash_func(p, prime, x);
+	string t_substr = t.substr(text_size - pattern_size, pattern_size);
+
+	std::vector<ull> H = PrecomputeHash(t, t_substr, pattern_size, prime, x);
+	
+	//t_substr = t.substr(0, pattern_size);
+	for (int i = 0 ; i <= text_size - pattern_size; ++i)
+	{
+		//t_substr = t.substr(i, pattern_size);
+		if ((H[i] == pattern_hash) && (t.substr(i, pattern_size).compare(p) == 0)) {
+				//ans.push_back(i);
+			std::cout << i << " ";
+		}
+		
+		//t_substr = t[i - 1] + t_substr.erase(pattern_size - 1, 1);
+		//t_substr = t_substr.erase(0, 1) + t[i + pattern_size];
+		//t_substr = t[0:3];
+		//t.substr(i, pattern_size);
+	}
+
+	/*
+	if ((H[text_size - pattern_size] == pattern_hash) && (t_substr.compare(p) == 0)) {
+		//ans.push_back(0);
+		std::cout << (text_size - pattern_size) << " ";
+	}
+	*/
+	std::cout << std::endl;
+
+	//return ans;
+
+}
+
+int main() 
+{
+	//high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    std::ios_base::sync_with_stdio(false);
+    //print_occurrences(RibinKarp(read_input()));
+	RibinKarp(read_input());
+	//high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+	//double duration = double(duration_cast<microseconds>(t2 - t1).count()/1000000);
+
+	//std::cout << duration << std::endl;
     return 0;
 }
